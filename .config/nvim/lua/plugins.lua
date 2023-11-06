9e9bd14f896e61444feb266d1cc506ddda198cd3
Michael Welford <mail@michaelwelford.com>
2023-11-07 09:05:56 +1030

    Fix spectre, again.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -125,17 +125,6 @@ return {
     },
   },
 
-  -- Replace
-  {
-    "nvim-pack/nvim-spectre",
-    cmd = "Spectre",
-    opts = { open_cmd = "noswapfile vnew" },
-    -- stylua: ignore
-    keys = {
-      { '<leader>sr', function() require('spectre').open() end, desc = 'Replace in files' },
-    },
-  },
-
   -- Statusline, see lualine.lua file
 
   -- Sign column, see signs.lua file
