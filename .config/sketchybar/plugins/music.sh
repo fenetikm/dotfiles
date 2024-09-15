d4ad26ec13087df279e0ca7e2b9dddc9823c92c5
Michael Welford <mail@michaelwelford.com>
2024-09-15 16:58:17 +0930

    Trying music scrolling.

--- .config/sketchybar/plugins/music.sh
+++ .config/sketchybar/plugins/music.sh
@@ -2,6 +2,8 @@
 
 source "$HOME/.config/sketchybar/vars.sh"
 
+DO_SCROLL=on
+DO_TRIM=off
 MAX_LEN=28
 TRIM_LEN=`echo $(( $MAX_LEN - 3 ))`
 
@@ -32,16 +34,20 @@ EOF)
 
   FULL="${MUSIC_TRACK} - ${MUSIC_ARTIST}"
   FULL_LEN=${#FULL}
-  if [[ "$FULL_LEN" > "$TRIM_LEN" ]]; then
+  if [[ "$FULL_LEN" > "$TRIM_LEN" && "$DO_TRIM" == "on" ]]; then
     FULL=`echo "$FULL" | cut -c -$TRIM_LEN`
     FULL+=...
   fi
+
+  SCROLL_DURATION=`echo $(( ($FULL_LEN - $MAX_LEN) * 10 ))`
 
   ICON="󰺢 "
   sketchybar --set "$NAME" icon="$ICON" label="${FULL}" \
     label.padding_right=10 \
     drawing=on \
     label.max_chars=$MAX_LEN \
+    scroll_texts="$DO_SCROLL" \
+    label.scroll_duration="$SCROLL_DURATION" \
     icon.padding_left=10 icon.color=$ICON_COLOUR \
     background.drawing=on background.color=$BG1_COLOUR \
     background.shadow.drawing=on background.shadow.distance=1 \
