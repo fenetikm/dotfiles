05fcd14f9f75f0a6168b33db56c174c7d868991b
Michael Welford <mail@michaelwelford.com>
2024-09-25 13:19:30 +0930

    Fix balance.sh for widescreen vs ultra.

--- .config/yabai/balance.sh
+++ .config/yabai/balance.sh
@@ -7,8 +7,14 @@ WINDOWS=($(yabai -m query --windows --space | jq '[.[] | select(."is-visible" ==
 WINDOW_COUNT=${#WINDOWS}
 DISPLAY=$(yabai -m query --displays --space | jq '.index')
 
-if [[ "$WINDOW_COUNT" == 2 && "$DISPLAY" == 2 ]]; then
+DISPLAY_WIDTH=$(yabai -m query --displays --space | jq '.frame.w')
+# this should really work out the ratio instead of only looking at the width
+IS_ULTRAWIDE=$(echo "$DISPLAY_WIDTH > 3000" | bc -l)
+
+if [[ "$WINDOW_COUNT" == 2 && "$DISPLAY" == 2 && "$IS_ULTRAWIDE" == 1 ]]; then
   yabai -m window "$WINDOWS[1]" --ratio "abs:0.333334"
+elif [[ "$IS_ULTRAWIDE" == 0 ]]; then
+  yabai -m space --balance
 elif [[ "$WINDOW_COUNT" > 2 && "$DISPLAY" == 2 ]]; then
   yabai -m space --balance
 elif [[ "$DISPLAY" != 2 ]]; then
