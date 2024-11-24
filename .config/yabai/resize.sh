c1c32d3f106e9a1781618d1a34fb4b6b73054534
Michael Welford <mail@michaelwelford.com>
2024-11-25 09:18:32 +1030

    More on resize.

--- .config/yabai/resize.sh
+++ .config/yabai/resize.sh
@@ -6,7 +6,7 @@
 # - 13 = 1/3
 # - 12 = 1/2
 # - 23 = 2/3
-# - 23s = smaller 2/3
+# - 23s = smaller 2/3, lol what is this for?
 # - wwww,hhhh = specific size
 # positions:
 # - 1,2,3 (thirds)
@@ -21,9 +21,26 @@
 
 W=$(yabai -m query --windows --window)
 D=$(yabai -m query --displays --display)
+GAP=20
+BAR_HEIGHT=32
+PADDING=20
 WID=$(echo "$W" | jq '.id')
-echo "$W" >> "/tmp/yabai_michael.out.log"
-echo "$WID" >> "/tmp/yabai_michael.out.log"
+DISPLAY_WIDTH=$(echo "$D" | jq '.frame.w | floor')
+DISPLAY_HEIGHT=$(echo "$D" | jq '.frame.h | floor')
+DISPLAY_X=$(echo "$D" | jq '.frame.x | floor')
+DISPLAY_IDX=$(echo "$W" | jq '.display')
+WINDOW_WIDTH=$(echo "$W" | jq '.frame.w | floor')
+WINDOW_HEIGHT=$(echo "$W" | jq '.frame.h | floor')
+
+POSITION="$1"
+if [[ "$POSITION" == "" ]]; then
+  POSITION=1
+fi
+
+SIZE="$2"
+if [[ "$SIZE" == "" ]]; then
+  SIZE=1
+fi
 
 DO_FLOAT="$3"
 if [[ "$DO_FLOAT" == "" ]]; then
@@ -34,58 +51,58 @@ if [[ $(echo "$W" | jq -re '."is-floating"') == false && "$DO_FLOAT" == 1 ]]; th
   yabai -m window "$WID" --toggle float
 fi
 
-DISPLAY_WIDTH=$(echo "$D" | jq '.frame.w | floor')
-DISPLAY_HEIGHT=$(echo "$D" | jq '.frame.h | floor')
-DISPLAY_X=$(echo "$D" | jq '.frame.x | floor')
-DISPLAY_IDX=$(echo "$W" | jq '.display')
-
-WINDOW_WIDTH=$(echo "$W" | jq '.frame.w | floor')
-WINDOW_HEIGHT=$(echo "$W" | jq '.frame.h | floor')
-
-# echo "$1" >> "/tmp/yabai_michael.out.log"
-# echo "$2" >> "/tmp/yabai_michael.out.log"
-# echo "$3" >> "/tmp/yabai_michael.out.log"
-
 resize_window() {
-  echo $1
+  if [[ "$1" == "x" ]]; then
+    # do nothing
+  elif [[ "$1" =~ ".,." ]]; then
+    WINDOW_WIDTH=$(echo "$1" | cut -d "," -f 1)
+    WINDOW_HEIGHT=$(echo "$1" | cut -d "," -f 2)
 
-
-  exit 1
-  # yabai -m window "$WID" --resize abs:"$1":"$2"
+    yabai -m window "$WID" --resize abs:"$WINDOW_WIDTH":"$WINDOW_HEIGHT"
+  elif [[ "$1" == "12" ]]; then
+    WINDOW_WIDTH=$(( "$WINDOW_WIDTH" / 2))
+    yabai -m window --grid '1:2:0:0:1:1'
+  elif [[ "$1" == "13" ]]; then
+    WINDOW_WIDTH=$(( "$WINDOW_WIDTH" / 3))
+    yabai -m window --grid '12:12:0:0:3:12'
+  elif [[ "$1" == "23" ]]; then
+    WINDOW_WIDTH=$(( "$WINDOW_WIDTH" / 3 * 2))
+    yabai -m window --grid '12:12:0:0:9:12'
+  fi
 }
 
-echo "$2"
+position_window() {
+  # Current assumption here is that display 2 is on the left of display 1
+  X_OFFSET=0
+  if [[ "$DISPLAY_IDX" == 2 ]]; then
+    X_OFFSET=$((0 - "$DISPLAY_WIDTH"))
+  fi
+  X_OFFSET=$(( "$X_OFFSET" + "$LEFT" ))
+  Y_OFFSET="$TOP"
+  # centre window
+  if [[ "$1" == "c" ]]; then
+    echo 'centre'
+    NEW_X=$(( "$X_OFFSET" + (("$DISPLAY_WIDTH" - "$WINDOW_WIDTH") / 2) ))
+    NEW_Y=$(( "$Y_OFFSET" + (("$DISPLAY_HEIGHT" - "$WINDOW_HEIGHT") / 2) ))
+    echo $DISPLAY_WIDTH
+    echo $DISPLAY_HEIGHT
+    echo $WINDOW_WIDTH
+    echo $WINDOW_HEIGHT
+    echo $NEW_X
+    echo $NEW_Y
+
+    # yabai -m window "$WID" --move abs:$NEW_X:$NEW_Y
+  elif [[ "$1" == "1" || "$1" == "2" || "$1" == "3" ]]; then
+    echo 'thirds'
+    POS=$(( "$1" - 1))
+    yabai -m window --grid '1:3:'"$POS"'0:1:1'
+  fi
+
+  exit 1
+}
 
 resize_window "$2"
-
-POSITION="$1"
-if [[ "$POSITION" == "" ]]; then
-  POSITION=1
-fi
-
-if [[ "$POSITION" == "c" ]]; then
-  if [[ "$DISPLAY_IDX" == 2 ]]; then
-    NEW_X=$(( 0 - "$DISPLAY_WIDTH" + (("$DISPLAY_WIDTH" - "$WINDOW_WIDTH") / 2) ))
-    NEW_Y=$(( ("$DISPLAY_HEIGHT" - "$WINDOW_HEIGHT") / 2 ))
-
-    yabai -m window "$WID" --move abs:$NEW_X:$NEW_Y
-
-    exit 1
-  fi
-  if [[ "$DISPLAY_IDX" == "1" ]];
-    NEW_X=$(( ("$DISPLAY_WIDTH" - "$WINDOW_WIDTH") / 2 ))
-    NEW_Y=$(( ("$DISPLAY_HEIGHT" - "$WINDOW_HEIGHT") / 2 ))
-
-    yabai -m window "$WID" --move abs:$NEW_X:$NEW_Y
-
-    exit 1
-  fi
-fi
-
-SIZE="$2"
-if [[ "$SIZE" == "" ]]; then
-  SIZE=1
-fi
+position_window "$1"
 
 # DISPLAY_IDX=$(yabai -m query --windows --window | jq '.display')
 # if [[ "$DISPLAY_IDX" == 1 ]]; then
