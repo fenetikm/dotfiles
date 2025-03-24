3557c56287235e60e31ddc04ae919c134d03ea04
Michael Welford <mail@michaelwelford.com>
2025-03-24 20:14:39 +1030

    More toggl.

--- .config/scripts/toggl.sh
+++ .config/scripts/toggl.sh
@@ -4,40 +4,62 @@
 
 # todo:
 # - handle bad token / auth issues
-# - diff colour depending on project
-# - project and task caching
+# - handle no timer running
+# - diff colour depending on project - supported
+# - project and task caching - done
+
+PROJECT_NAME=
+PROJECT_COLOUR=
+TASK_NAME=
 
 get_project() {
   local WORKSPACE_ID=$1
   local PROJECT_ID=$2
-  local PROJECT=$(curl -s -u "$TOGGLAPIKEY":api_token \
-    -H "Content-Type: application/json" \
-    -X GET https://api.track.toggl.com/api/v9/workspaces/"$WORKSPACE_ID"/projects/"$PROJECT_ID")
-  local PROJECT_NAME=$(echo "$PROJECT" | jq -r .name)
-  local PROJECT_COLOUR=$(echo "$PROJECT" | jq -r .color)
+  if [[ ! -f "project_$PROJECT_ID.json" ]]; then
+    curl -s -u "$TOGGLAPIKEY":api_token \
+      -o "project_$PROJECT_ID.json" \
+      -H "Content-Type: application/json" \
+      -X GET https://api.track.toggl.com/api/v9/workspaces/"$WORKSPACE_ID"/projects/"$PROJECT_ID"
+  fi
+  local PROJECT=$(cat "project_$PROJECT_ID.json")
+  PROJECT_NAME=$(echo "$PROJECT" | jq -r .name)
+  PROJECT_COLOUR=$(echo "$PROJECT" | jq -r .color)
 }
 
 get_task() {
   local WORKSPACE_ID=$1
   local PROJECT_ID=$2
   local TASK_ID=$3
-  local TASK=$(curl -s -u "$TOGGLAPIKEY":api_token \
-    -H "Content-Type: application/json" \
-    -X GET https://api.track.toggl.com/api/v9/workspaces/"$WORKSPACE_ID"/projects/"$PROJECT_ID"/tasks/"$TASK_ID")
-      local TASK_NAME=$(echo "$TASK" | jq -r .name)
+  if [[ ! -f "task_$TASK_ID.json" ]]; then
+    curl -s -u "$TOGGLAPIKEY":api_token \
+      -o "task_$TASK_ID.json" \
+      -H "Content-Type: application/json" \
+      -X GET https://api.track.toggl.com/api/v9/workspaces/"$WORKSPACE_ID"/projects/"$PROJECT_ID"/tasks/"$TASK_ID"
+  fi
+  local TASK=$(cat "task_$TASK_ID.json")
+  TASK_NAME=$(echo "$TASK" | jq -r .name)
 }
 
 get_current() {
   local CURRENT=$(curl -s -u "$TOGGLAPIKEY":api_token \
       -H "Content-Type: application/json" \
       -X GET https://api.track.toggl.com/api/v9/me/time_entries/current)
+  if [[ "$CURRENT" == "null" ]]; then
+    return
+  fi
 
   local WORKSPACE_ID=$(echo "$CURRENT" | jq -r .workspace_id)
   local PROJECT_ID=$(echo "$CURRENT" | jq -r .project_id)
   local TASK_ID=$(echo "$CURRENT" | jq -r .task_id)
+  local DESCRIPTION=$(echo "$CURRENT" | jq -r .description)
 
-  # echo $(get_project "$WORKSPACE_ID" "$PROJECT_ID")
-  echo $(get_task "$WORKSPACE_ID" "$PROJECT_ID" "$TASK_ID")
+  get_project "$WORKSPACE_ID" "$PROJECT_ID"
+  get_task "$WORKSPACE_ID" "$PROJECT_ID" "$TASK_ID"
+
+  echo "$PROJECT_NAME"
+  echo "$PROJECT_COLOUR"
+  echo "$TASK_NAME"
+  echo "$DESCRIPTION"
 
   local START=$(echo "$CURRENT" | jq -r .start | gsed 's/00:00/0000/')
   local NOW=$(date +"%s")
