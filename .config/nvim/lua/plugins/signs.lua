dd0bcad56464f016995631f8ac60c44d940e8adf
Michael Welford <mail@michaelwelford.com>
2025-06-13 20:45:15 +0930

    Various, plugin updates.

--- .config/nvim/lua/plugins/signs.lua
+++ .config/nvim/lua/plugins/signs.lua
@@ -4,7 +4,7 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
       {
-        "seanbreckenridge/gitsigns-yadm.nvim",
+        "purarue/gitsigns-yadm.nvim",
         opts = {
             shell_timeout_ms = 1000,
         },
