3818a9771fd45ecf87aca0e8602165ace03bf9ce
Michael Welford <mail@michaelwelford.com>
2025-06-17 20:00:50 +0930

    Add in sesh.sh.

---
+++ .config/tmux/sesh.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+
+# usage:
+# - $1: session name, defaults to directory name
+# - $2: target directory, defaults to current directory
+
+switch_to_session() {
+    if [[ -z "$TMUX" ]]; then
+        tmux attach-session -t $1
+    else
+        tmux switch-client -t $1
+    fi
+}
+
+SESSION_NAME=$1
+if [[ "$SESSION_NAME" == "" ]]; then
+  # special case if we are in the home directory
+  if [[ "$HOME" == $(pwd) ]]; then
+    SESSION_NAME="z"
+  else
+    SESSION_NAME=$(basename $(pwd) | tr ".,: " "____")
+  fi
+fi
+
+DIR=$2
+if [[ "$DIR" == "" ]]; then
+  DIR=$(pwd)
+fi
+
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+  switch_to_session $SESSION_NAME
+else
+  tmux new-session -ds "$SESSION_NAME" -n setup -c "$DIR"
+  tmux select-window -t "$SESSION_NAME":setup
+  tmux select-pane -t 1
+  tmux send-keys -t "$SESSION_NAME":setup "tmux-setup $SESSION_NAME" C-m
+  switch_to_session "$SESSION_NAME"
+fi
