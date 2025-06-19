91128b801e39e4f12da6f1e6c6bea4d200534724
Michael Welford <mail@michaelwelford.com>
2025-06-19 20:55:06 +0930

    Add more hooks.

--- .config/tmux/tree_rebuild.sh
+++ .config/tmux/tree_rebuild.sh
@@ -6,6 +6,7 @@ CACHE_LOC="$HOME/.config/tmux/tree_cache.txt"
 CACHE_BUILD="$HOME/.config/tmux/tree.sh"
 
 # only build cache if it doesn't exist or older than an hour
+# note: "FORCE" has no $, doesn't work inside arithmetic context
 if [[ ! -f "$CACHE_LOC" ]] || (( "FORCE" == "1" )); then
   "$CACHE_BUILD" > "$CACHE_LOC"
 elif [[ $(find "$CACHE_LOC" -mtime +1h -print) ]]; then
