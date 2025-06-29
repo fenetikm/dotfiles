4bf3013ca472d05124264771e5d38223f534825a
Michael Welford <mail@michaelwelford.com>
2025-06-30 08:23:30 +0930

    Add in no symbols sh for sketchy.

---
+++ .config/sketchybar/plugins/space_no_symbols.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+source "$HOME/.config/sketchybar/vars.sh"
+
+#todo make clickable
+SPACE_ICON=
+if [[ "$SELECTED" == "true" ]]; then
+  case "$NAME" in
+    space.1) SPACE_ICON="󰋜 " ;;
+    space.2) SPACE_ICON="󰇮 " ;;
+    space.3) SPACE_ICON="󰭹 " ;;
+    space.4) SPACE_ICON="󰆼 " ;;
+    space.5) SPACE_ICON="󰧚 " ;;
+    space.6) SPACE_ICON="󰈈 " ;;
+  esac
+else
+  case "$NAME" in
+    space.1) SPACE_ICON=" " ;;
+    space.2) SPACE_ICON=" " ;;
+    space.3) SPACE_ICON="󰻞 " ;;
+    space.4) SPACE_ICON="󱘲 " ;;
+    space.5) SPACE_ICON="󰧛 " ;;
+    space.6) SPACE_ICON="󰛐 " ;;
+  esac
+fi
+
+if [[ "$SELECTED" == "true" ]]; then
+  sketchybar --set "$NAME" label.color="$SELECTED_COLOUR"
+else
+  sketchybar --set "$NAME" label.color="$ICON_COLOUR"
+fi
