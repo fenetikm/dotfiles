af1b17ebc908c583d6061a249d607ade5486b6b6
Michael Welford <mail@michaelwelford.com>
2025-07-02 10:20:39 +0930

    Fix yabai setup_space, now shifts wrong space / display correctly.

--- .config/sketchybar/plugins/cpu_load.sh
+++ .config/sketchybar/plugins/cpu_load.sh
@@ -10,18 +10,19 @@ THRESHOLD=12
 PERC=$(/usr/bin/top -l 1 | grep -E "^CPU" | grep -Eo '[^[:space:]]+%' | head -1)
 PERC=$(echo "$PERC" | sed -E 's/([0-9])([0-9])\%/\1%/')
 
-COLOUR=$DEFAULT_COLOUR
+COLOUR="$DEFAULT_COLOUR"
 
 ROUNDED_PERC=$(echo "$PERC" | sed -E 's/(.*)(\.[0-9]\%)/\1/')
-if (( $ROUNDED_PERC > $THRESHOLD )); then
-  COLOUR=$WARNING_COLOUR
+if (( "$ROUNDED_PERC" > "$THRESHOLD" )); then
+  COLOUR="$WARNING_COLOUR"
 fi
-if (( $ROUNDED_PERC > 20 )); then
-  COLOUR=$ISSUE_COLOUR
+
+if (( "$ROUNDED_PERC" > 20 )); then
+  COLOUR="$ISSUE_COLOUR"
 fi
 
 if (( $ROUNDED_PERC <= $THRESHOLD )); then
   sketchybar --set "$NAME" drawing=off
 else
-  sketchybar --set "$NAME" label.color="${COLOUR}" label="C:${PERC}" drawing=on
+  sketchybar --set "$NAME" label.color="${COLOUR}" label="C:${PERC}" drawing=on icon.drawing=off
 fi
