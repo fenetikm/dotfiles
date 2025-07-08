d9cd5bbb1275a95635403615520ebddff021b1c9
Michael Welford <mail@michaelwelford.com>
2025-07-08 10:21:06 +0930

    Yabai and toggle mode.

---
+++ .config/yabai/present.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+/bin/zsh "$HOME/.config/yabai/send_space.sh" 6 1
+/bin/zsh "$HOME/.config/yabai/resize.sh" c 1600,1200 1
---
+++ .config/yabai/toggle_mode.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+MODE=$(yabai -m query --spaces --space | jq -r '.type')
+SPACE_INDEX=$(yabai -m query --spaces --space | jq -r '.index')
+
+if [[ "$MODE" == "bsp" ]]; then
+  yabai -m space --layout stack
+else
+  yabai -m space --layout bsp
+  /bin/zsh "$HOME/.config/yabai/reset_opacity.sh" "$SPACE_INDEX"
+fi
