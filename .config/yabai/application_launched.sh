37b238ffd74d1a2e03f0cfac98f9af2b6b9d47e6
Michael Welford <mail@michaelwelford.com>
2025-07-21 21:25:55 +0930

    Always hide transmission.

--- .config/yabai/application_launched.sh
+++ .config/yabai/application_launched.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env zsh
+#!/usr/bin/env zsh
 
 source "$HOME"/.config/yabai/tools.sh
 
--- .config/yabai/tools.sh
+++ .config/yabai/tools.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 yd() {
   echo '################' >> /tmp/yabai_michael.out.log
