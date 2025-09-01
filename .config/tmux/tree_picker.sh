92c3c98b61ec5b448bc8fc39b411eb8e25f8935f
Michael Welford <mail@michaelwelford.com>
2025-09-01 20:20:23 +0930

    Hide more tree picker stuff.

--- .config/tmux/tree_picker.sh
+++ .config/tmux/tree_picker.sh
@@ -4,16 +4,15 @@
 
 CLIENT_WIDTH=$(tmux display-message -p '#{client_width}')
 
-# todo: put tmp files in temp
 if (( "$CLIENT_WIDTH" < 160 )); then
   # no preview when not much space
   cat "$HOME/.config/tmux/tmp_tree_cache.txt" |
-    fzf --color=bg:#020223,bg+:#020223 --reverse --ansi |
+    fzf --color=bg:#020223,bg+:#020223 --no-scrollbar --no-info --reverse --ansi |
       sed -E 's/(.*)\((.*)\)/\2/' |
       xargs tmux switch-client -t
 else
   cat "$HOME/.config/tmux/tmp_tree_cache.txt" |
-    fzf --color=bg:#020223,bg+:#020223 --reverse --ansi --preview "echo {} | sed -E 's/(.*)\((.*)\)/\2.1/' | xargs tmux capture-pane -p -t" |
+    fzf --color=bg:#020223,bg+:#020223 --no-scrollbar --no-info --reverse --ansi --preview-window=right,noinfo --preview "echo {} | sed -E 's/(.*)\((.*)\)/\2.1/' | xargs tmux capture-pane -p -t" |
       sed -E 's/(.*)\((.*)\)/\2/' |
       xargs tmux switch-client -t
 fi
