854e9290b38a9980b449338d5e5ce4eb60e02aa7
Michael Welford <mail@michaelwelford.com>
2025-09-10 19:53:48 +0930

    Various tweaks.

--- .config/sketchybar/plugins/volume.sh
+++ .config/sketchybar/plugins/volume.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
+# todo: show when it changes, then hide after a second
+# ...do this with a sleep / wait?
+# ... create semaphore?
+
 # $INFO has the volume percentage
-if [ "$SENDER" = "volume_change" ]; then
+if [[ "$SENDER" = "volume_change" ]]; then
   VOLUME="$INFO"
 
   sketchybar --set "$NAME" label="V:$VOLUME" icon.drawing=off padding_right=4
+
 fi
