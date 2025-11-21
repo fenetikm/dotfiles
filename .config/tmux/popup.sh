6de906fdf08a88f380085021eaa66c54604943ff
Michael Welford <mail@michaelwelford.com>
2025-11-21 10:56:18 +1030

    fix: smart popup sizing.

--- .config/tmux/popup.sh
+++ .config/tmux/popup.sh
@@ -21,6 +21,30 @@ if [[ "$2" == "" ]]; then
   TYPE="ephemeral"
 fi
 
+# "smart" sizing
+PERC=70
+MIN_WIDTH=130
+MIN_HEIGHT=50
+CURRENT_WIDTH=$(tmux display -p "#{window_width}")
+CURRENT_HEIGHT=$(tmux display -p "#{window_height}")
+MARGIN=10
+WIDTH=$(( CURRENT_WIDTH * PERC / 100 ))
+if (( WIDTH < MIN_WIDTH )); then
+  if (( MIN_WIDTH + MARGIN > CURRENT_WIDTH )); then
+    WIDTH=$(( CURRENT_WIDTH - MARGIN ))
+  else
+    WIDTH=$(( MIN_WIDTH ))
+  fi
+fi
+HEIGHT=$(( CURRENT_HEIGHT * PERC / 100 ))
+if (( HEIGHT < MIN_HEIGHT )); then
+  if (( MIN_HEIGHT + MARGIN > CURRENT_HEIGHT )); then
+    HEIGHT=$(( CURRENT_HEIGHT - MARGIN ))
+  else
+    HEIGHT=$(( MIN_HEIGHT ))
+  fi
+fi
+
 if [[ "$TYPE" == "persistent" ]]; then
   # detach / hide if already visible
   if [[ "$(tmux display-message -p -F "#{session_name}")" = "$SESSION" ]]; then
@@ -28,7 +52,7 @@ if [[ "$TYPE" == "persistent" ]]; then
   else
     # see .tmux.conf.popup for options set on the popup
     # set via the `after-new-session` hook
-    tmux display-popup -d '#{pane_current_path}' -b rounded -w 70% -h 70% -s "bg=#020223" -E "tmux attach -t $SESSION || tmux new -s $SESSION"
+    tmux display-popup -d '#{pane_current_path}' -b rounded -w "$WIDTH" -h "$HEIGHT" -s "bg=#020223" -E "tmux attach -t $SESSION || tmux new -s $SESSION"
   fi
 else
 fi
