200225ed85b92fec53f07d0e11955ccdd765e2dd
Michael Welford <mail@michaelwelford.com>
2026-01-23 11:04:07 +1030

    fix: project selector in tmux.

--- .config/tmux/projects.sh
+++ .config/tmux/projects.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
-ls -d -1 ~/Documents/Work/internal/projects/* |
-  fzf |
-  read f && echo "$f" |
-  sed 's|.*/||' |
-  xargs -I {} "$HOME"/.config/tmux/sesh.sh auto {} "$f"
+FILE=$(ls -d -1 ~/Documents/Work/internal/projects/* |
+  fzf --no-multi)
+if [[ "$FILE" != "" ]]; then
+  "$HOME"/.config/tmux/sesh.sh auto "$FILE"
+fi
 
