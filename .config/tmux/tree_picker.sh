e0622e991ce449c68d96ce4595bfbc3c6da830fd
Michael Welford <mail@michaelwelford.com>
2026-02-16 08:34:25 +1030

    fix: add in reload to tmux tree picker.

--- .config/tmux/tree_picker.sh
+++ .config/tmux/tree_picker.sh
@@ -7,12 +7,28 @@ CLIENT_WIDTH=$(tmux display-message -p '#{client_width}')
 if (( "$CLIENT_WIDTH" < 160 )); then
   # no preview when not much space
   cat "$HOME/.config/tmux/tmp_tree_cache.txt" |
-    fzf --color=bg:#020223,bg+:#020223 --no-scrollbar --no-info --reverse --ansi |
+    fzf \
+      --color=bg:#020223,bg+:#020223 \
+      --no-scrollbar \
+      --no-info \
+      --reverse \
+      --bind 'ctrl-r:execute-silent("$HOME/.config/tmux/tree_rebuild.sh 1")' \
+      --bind 'ctrl-r:+reload(cat "$HOME/.config/tmux/tmp_tree_cache.txt")' \
+      --ansi |
       sed -E 's/(.*)\((.*)\)/\2/' |
       xargs tmux switch-client -t
 else
   cat "$HOME/.config/tmux/tmp_tree_cache.txt" |
-    fzf --color=bg:#020223,bg+:#020223 --no-scrollbar --no-info --reverse --ansi --preview-window=right,noinfo --preview "echo {} | sed -E 's/(.*)\((.*)\)/\2.1/' | xargs tmux capture-pane -p -t" |
+    fzf \
+      --color=bg:#020223,bg+:#020223 \
+      --no-scrollbar \
+      --no-info \
+      --reverse \
+      --preview-window=right,noinfo \
+      --preview "echo {} | sed -E 's/(.*)\((.*)\)/\2.1/' | xargs tmux capture-pane -p -t" \
+      --bind 'ctrl-r:execute-silent("$HOME/.config/tmux/tree_rebuild.sh 1")' \
+      --bind 'ctrl-r:+reload(cat "$HOME/.config/tmux/tmp_tree_cache.txt")' \
+      --ansi |
       sed -E 's/(.*)\((.*)\)/\2/' |
       xargs tmux switch-client -t
 fi
