a0b77a0471a7c05d4251e062759af011507ff322
Michael Welford <mail@michaelwelford.com>
2026-03-15 21:28:11 +1030

    feat: diffview directory watching.

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,3 +1,43 @@
+-- from: https://github.com/richardgill/nix/blob/ebdd826/modules/home-manager/dot-files/nvim/lua/plugins/git-diff_diffview.lua
+-- refreshes diffview when a change is made to a file
+local is_git_ignored = function(filepath)
+  vim.fn.system('git check-ignore -q ' .. vim.fn.shellescape(filepath))
+  return vim.v.shell_error == 0
+end
+
+local update_left_pane = function()
+  pcall(function()
+    local lib = require 'diffview.lib'
+    local view = lib.get_current_view()
+    if view then
+      -- This updates the left panel with all the files, but doesn't update the buffers
+      view:update_files()
+    end
+  end)
+end
+
+vim.notify '[diffview] init'
+-- Register handler for file changes in watched directory
+require('custom.directory-watcher').registerOnChangeHandler('diffview', function(filepath, events)
+  local is_in_dot_git_dir = filepath:match '/%.git/' or filepath:match '^%.git/'
+
+  if is_in_dot_git_dir or not is_git_ignored(filepath) then
+    vim.notify('[diffview] File changed: ' .. vim.fn.fnamemodify(filepath, ':t'), vim.log.levels.INFO)
+    update_left_pane()
+  end
+end)
+
+vim.api.nvim_create_autocmd('FocusGained', {
+  callback = update_left_pane,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'DiffviewViewLeave',
+  callback = function()
+    vim.cmd ':DiffviewClose'
+  end,
+})
+
 return {
   {
     'tpope/vim-fugitive',
