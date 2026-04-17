7666d53b584ace8a3f9c93f2efba28ec0b6e46e3
Michael Welford <mail@michaelwelford.com>
2026-04-17 14:00:20 +0930

    fix(nv): sort out markdown highglighting.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,17 +5,31 @@ return {
     build = ':TSUpdate',
     -- event = { 'VeryLazy' },
     init = function()
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-      -- during startup.
-      -- require("lazy.core.loader").add_to_rtp(plugin)
-      -- require("nvim-treesitter.query_predicates")
+      -- use treesitter on these
       vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'bash', 'c', 'cpp', 'css', 'go', 'html',
+          'javascript', 'json', 'lua', 'python',
+          'rust', 'toml', 'typescript', 'elixir', 'php', 'java',
+          'ruby', 'twig',
+          'http',
+        },
         callback = function()
           -- Enable treesitter highlighting and disable regex syntax
           pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+
+      -- use regex matching on these
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'markdown', 'markdown_inline',
+        },
+        callback = function()
+          -- Disable treesitter highlighting
+          pcall(vim.treesitter.stop)
           -- Enable treesitter-based indentation
           vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end,
@@ -50,30 +64,6 @@ return {
       { "<c-space>", desc = "Increment selection" },
       { "<bs>",      desc = "Decrement selection", mode = "x" },
     },
-    config = function()
-      local ts_configs = require('nvim-treesitter')
-      ts_configs.setup {
-        -- context_commentstring = { enable = true, enable_autocmd = false },
-        -- incremental_selection = {
-        --   enable = true,
-        --   keymaps = {
-        --     init_selection = "<C-space>",
-        --     node_incremental = "<C-space>",
-        --     scope_incremental = false,
-        --     node_decremental = "<bs>",
-        --   },
-        -- },
-        -- textobjects = {
-        --   move = {
-        --     enable = true,
-        --     goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-        --     goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-        --     goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-        --     goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-        --   },
-        -- },
-      }
-    end
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
