2ec9a8fad31633789f269c6c022a98c5a1347d0d
Michael Welford <mail@michaelwelford.com>
2026-05-05 12:30:35 +0930

    chore(zsh): more dir hash cleanup.

--- .config/zsh/directory_hashes.sh
+++ .config/zsh/directory_hashes.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env zsh
 
-hash -d sys=~/Documents/Work/internal/vms/sys
 hash -d blog=~/Documents/Work/internal/blog/hugo/mwcom
-hash -d i=~/Documents/Work/internal
 hash -d w=~/Documents/Work
 hash -d docs=~/Documents
 hash -d down=~/Downloads
-hash -d p=~/Documents/personal
 hash -d falcon=~/Documents/Work/internal/vim/colors/falcon
 hash -d nv=~/.config/nvim
 hash -d z=~/vaults/zettelkasten/
 hash -d icloud=~/Library/Mobile\ Documents/com~apple~CloudDocs/
 hash -d yt=~/Downloads/yt
-hash -d crafting=~/Documents/Work/internal/projects/interpreters/crafting
 hash -d m=~/Documents/Work/internal/projects/majyk
 hash -d y=~/.config/yabai
 hash -d sb=~/.config/sketchybar
