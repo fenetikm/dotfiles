3e10ea0aa4b4fa24cd305aa8c09b3053c75d11e4
Michael Welford <mail@michaelwelford.com>
2026-08-24 08:10:02 +0930

    chore(zsh): clean up fzf functions

--- .config/zsh/fzf.sh
+++ .config/zsh/fzf.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+# some general fzf magical functions
+
 # fzf for checking out a branch
 fzf_git_checkout() {
   result=$(git branch -a | grep -v '/HEAD\s' | sort |
@@ -14,47 +16,6 @@ fzf_git_checkout() {
       git checkout "$result"
     fi
   fi
-}
-
-# fzf git log
-fzf_git_log() {
-    local commits=$(
-      git ll --color=always "$@" |
-        fzf --no-sort --height 100% \
-            --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
-                       xargs -I@ sh -c 'git show --color=always @'"
-      )
-    if [[ -n $commits ]]; then
-        local hashes=$(printf "$commits" | cut -d' ' -f2 | tr '\n' ' ')
-        git show $hashes
-    fi
-}
-
-# fzf find a file by name and edit
-fzf_find_edit() {
-    local file=$(
-      fzf --no-multi --preview 'bat --color=always --line-range :500 {}'
-      )
-    if [[ -n $file ]]; then
-        $EDITOR $file
-    fi
-}
-
-# fzf find a file with text and edit
-fzf_grep_edit(){
-    if [[ $# == 0 ]]; then
-        echo 'Error: search term was not provided.'
-        return
-    fi
-    local match=$(
-      rg --color=never --line-number "$1" |
-        fzf --no-multi --delimiter : \
-            --preview "bat --color=always --line-range {2}: {1}"
-      )
-    local file=$(echo "$match" | cut -d':' -f1)
-    if [[ -n $file ]]; then
-        $EDITOR $file +$(echo "$match" | cut -d':' -f2)
-    fi
 }
 
 # fkill - kill processes - list only the ones you can kill. Modified the earlier script.
@@ -73,7 +34,4 @@ fzf_kill_process() {
 }
 
 alias fkill='fzf_kill_process'
-alias fge='fzf_grep_edit'
-alias ffe='fzf_find_edit'
-alias fgl="fzf_git_log"
 alias fco="fzf_git_checkout"
